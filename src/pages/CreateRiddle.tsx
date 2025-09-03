0b8fb38484f78c57a90798cbddd07bce5df24290
yoel ider <yoeli5746@gmail.com>
2025-09-03 17:02:58 +0300

    finish Auth

--- src/pages/CreateRiddle.tsx
+++ src/pages/CreateRiddle.tsx
@@ -1,11 +1,34 @@
+import { useRef } from "react";
+import { insertRiddleToLS } from "../util/LocalStorage";
+
 export default function CreateRiddle() {
+  const questionRef = useRef<HTMLInputElement>(null);
+  const answerRef = useRef<HTMLInputElement>(null);
   return (
     <div>
       <h1>Create a New Riddle</h1>
       <form action="">
-        <input type="text" placeholder="Enter riddle question" />
-        <input type="text" placeholder="Enter riddle answer" />
-        <button type="submit" onClick={(e) => {e.preventDefault(); alert("Riddle Created");}}>Create Riddle</button>
+        <input
+          type="text"
+          placeholder="Enter riddle question"
+          ref={questionRef}
+        />
+        <input type="text" placeholder="Enter riddle answer" ref={answerRef} />
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            if (!questionRef.current || !answerRef.current) return;
+            const riddle = {
+              question: questionRef.current.value,
+              answer: answerRef.current.value,
+            };
+            insertRiddleToLS("riddles", riddle);
+            alert("Riddle Created");
+          }}
+        >
+          Create Riddle
+        </button>
       </form>
     </div>
   );
--- src/pages/LoginOrSignUp.tsx
+++ src/pages/LoginOrSignUp.tsx
@@ -1,14 +1,36 @@
+import { useRef } from "react";
+// import { insertTokenToLS } from "../util/LocalStorage.ts";
 import { useNavigate } from "react-router";
+import { Auth } from "../util/LocalStorage.ts";
+
 
 export default function LoginOrSignUp() {
   const navigate = useNavigate();
+  const userNameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
   return (
     <div>
       <h1>Login or Sign Up</h1>
-      <input type="text" value={"enter username"} />
-      <input type="password" value={"enter password"} />
-      <button onClick={() => {navigate("/adminoruser")}}>Login</button>
-      <button onClick={() => {navigate("/adminoruser")}}>Sign Up</button>
+      <input type="text" placeholder="enter username" ref={userNameRef} />
+      <input type="password" placeholder="enter password" ref={passwordRef} />
+
+      <button
+        onClick={async (e) => {e.preventDefault();
+        const role=await Auth("siging","User created",userNameRef.current?.value,passwordRef.current?.value)
+        if(role.role==="admin"||"user"){
+          navigate("/adminoruser")
+        }
+        }}>Sign Up
+      </button>
+
+      <button
+        onClick={async (e) => {e.preventDefault();
+         const role=await Auth("login","Login successful",userNameRef.current?.value,passwordRef.current?.value)
+          if(role.role==="admin"||"user"){
+          navigate("/adminoruser")
+        }
+        }}>Login
+      </button>
     </div>
   );
 }
