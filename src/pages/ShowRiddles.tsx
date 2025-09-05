45553e53fd61bc237c22cce52d680ecf14009d1c
yoel-cmd <yoeli5746@gmail.com>
2025-09-05 08:46:04 +0300

    Update ShowRiddles.tsx

--- src/pages/ShowRiddles.tsx
+++ src/pages/ShowRiddles.tsx
@@ -17,27 +17,7 @@ export default function ShowRiddles() {
     fatchData();
   }, []);
 
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setTimer((prev: number) => prev + 1);
-  //   }, 1000);
-  // },[]);
-
-  //כאן אני עושה אפקט שמה שהוא עושה זה כל פעם שמשתנה המונה הוא עובר דרך פה ובודק אם נגמרו החידות
-
-  // useEffect(() => {//חייב פה לשים בפנים כי אי אפשר לעשות  אסינק
-  //   if (counter > riddle.length) {
-  //     navigate("/finish");
-  //   }
-  // }, [counter]);
-
-  // אם יש לי בדיקות שרק אם מתקיימות אני מציג  מסויימים
-  // if(true){
-  //   return <p>{counter<3 ?3:4}</p>
-  // } else {
-  //   return <h1></h1>
-  // };
-  console.log("counter is", counter);
+    console.log("counter is", counter);
   console.log("riddle.length:", riddle.length);
 
   if (counter >= riddle.length) {
